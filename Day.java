1fa64f9299f91dc32c61ccc66960bc1f52276ef1
samlewin19 <144487001+samlewin19@users.noreply.github.com>
2024-12-03 13:27:30 -0500

    Add files via upload

---
+++ Day.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Day {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/Day1.txt");
+    }
+
+        public static ArrayList<String> getFileData (String fileName){
+            ArrayList<String> fileData = new ArrayList<String>();
+            try {
+                File f = new File(fileName);
+                Scanner s = new Scanner(f);
+                while (s.hasNextLine()) {
+                    String line = s.nextLine();
+                    if (!line.equals(""))
+                        fileData.add(line);
+                }
+                return fileData;
+            } catch (FileNotFoundException e) {
+                return fileData;
+            }
+        }
+    }
