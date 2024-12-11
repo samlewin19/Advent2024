a3293ccb46275a89079d89acef104da6eaf00a85
samlewin19 <144487001+samlewin19@users.noreply.github.com>
2024-12-11 14:06:20 -0500

    Update Day5.java

--- Day5.java
+++ Day5.java
@@ -1,3 +1,5 @@
+import com.sun.jdi.ArrayReference;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -21,10 +23,69 @@ public class Day5 {
             rules[i][0]= Integer.valueOf(fileData.get(i).substring(0,val));
             rules[i][1]= Integer.valueOf(fileData.get(i).substring(val+1));
             //System.out.println(rules[i][0]);System.out.println(rules[i][1]);
-            }
-        //need to make a 2d array to hold all the prints and would like to use as manu assry lists as possible
-        ArrayList<Integer>[] prints = new ArrayList<Integer>[];
         }
+        //need to make a 2d array to hold all the prints and would like to use as manu assry lists as possible
+        ArrayList<ArrayList<Integer>> prints = new ArrayList<>();
+        for (int i = ite; i < fileData.size();i++){
+            String[] strlist = fileData.get(i).split(",");
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            for (int j = 0; j < strlist.length; j++){
+                temp.add(Integer.valueOf(strlist[j]));
+            }
+            prints.add(temp);
+        }
+        //System.out.println(prints);
+        int count = 0;
+
+        for(int i = 0; i < prints.size();i++){
+            ArrayList<ArrayList<Integer>> validRules = ruleCheck(rules, prints.get(i));
+            //System.out.println(validRules);
+            count+=midNum(validRules,prints.get(i));
+
+        }
+        System.out.println(count);
+
+    }
+    public static int midNum(ArrayList<ArrayList<Integer>> validRules, ArrayList<Integer> prints){
+        int mid = prints.get(prints.size()/2);
+        //System.out.println(mid);
+        for(int i = 0; i < prints.size();i++){
+            int num = prints.get(i);
+            for (int j = 0; j < validRules.size(); j++){
+                if(validRules.get(j).get(0) == num){
+                    int secondNum = validRules.get(j).get(1);
+                    for (int k = 0; k < prints.size(); k++){
+                        if (prints.get(k) == num){
+
+                        }
+                    }
+                }
+            }
+        }
+        return mid;
+    }
+    public static ArrayList ruleCheck(int[][] rules, ArrayList<Integer> prints){
+        ArrayList<ArrayList<Integer>> validRules = new ArrayList<>();
+        for(int i = 0; i < rules.length; i++){
+            if (isIn(rules[i], prints)) {
+                ArrayList<Integer> temp = new ArrayList<>();
+                temp.add(rules[i][0]);
+                temp.add((rules[i][1]));
+                validRules.add(temp);
+            }
+        }
+        return validRules;
+    }
+
+    public static boolean isIn(int[] nums, ArrayList<Integer> prints){
+        int count = 0;
+        for (int i = 0; i < prints.size();i++){
+            if (prints.get(i) == nums[0]) count++;
+            if (prints.get(i) == nums[1]) count++;
+        }
+        if (count >= 2) return true;
+        return false;
+    }
 
 
 
